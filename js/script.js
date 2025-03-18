67ef9f2a5b91bba3be58ecd4686ff994d820db3a
Akas_Datta <012akasdatta@gmail.com>
2025-03-19 04:01:55 +0600

    added video

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
+// Navbar code 
 const toggleButton = document.querySelector('label[for="menu-toggle"]');
 const navbarDefault = document.querySelector('.peer-checked + div');
 
 toggleButton.addEventListener('click', () => {
   navbarDefault.classList.toggle('hidden');
+});
+
+
+// Video code 
+const previewImage = document.getElementById('preview-image');
+const video = document.getElementById('my-video');
+
+previewImage.addEventListener('click', () => {
+    previewImage.style.display = 'none'; // hide image
+    video.classList.remove('hidden'); // show video
+    video.play(); // play video
 });
